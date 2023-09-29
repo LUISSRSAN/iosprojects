705aba56bc1a9e6156a020ab5f96a56125b850c8
LUIS  SANTIAGO <78453562+LUISSRSAN@users.noreply.github.com>
2023-09-29 14:14:43 -0400

    Add files via upload

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Luis Santiago  on 9/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
