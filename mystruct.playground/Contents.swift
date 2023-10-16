7993646dab23d6665a0b47e6c3268f5bd1a9a8d8
LUIS  SANTIAGO <78453562+LUISSRSAN@users.noreply.github.com>
2023-10-16 11:56:52 -0400

    Add files via upload

---
+++ mystruct.playground/Contents.swift
@@ -0,0 +1,21 @@
+struct Town {
+    let name :String
+    var citizens :[String]
+    var resources : [String:Int]
+    
+    init(townName:String,people:[String],stats:[String:Int]){
+        name = townName
+        citizens = people
+        resources = stats
+        
+    }
+    
+    func fortify(){
+        print("Defences increased!")
+    }
+    
+}
+
+var anotherTown = Town(townName:"Nameless Island", people: ["Tom Hanks"], stats:["Coconuts":100])
+anotherTown.citizens.append("Wilson")
+print(anotherTown.citizens)
